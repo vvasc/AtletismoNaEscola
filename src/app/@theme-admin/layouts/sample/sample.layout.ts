338d0c48658e7dd1615ef53dc2ca6de80ffd4c16
Tonhao165 <eugenio165.as@gmail.com>
2018-09-08 14:48:12 -0300

    160365777: Header tela principal e admin prontas

--- src/app/@theme-admin/layouts/sample/sample.layout.ts
+++ src/app/@theme-admin/layouts/sample/sample.layout.ts
@@ -20,6 +20,16 @@ import { StateService } from '../../../@core/data/state.service';
       <nb-layout-header fixed>
         <ngx-header [position]="sidebar.id === 'start' ? 'normal': 'inverse'"></ngx-header>
       </nb-layout-header>
+
+      <nb-sidebar class="menu-sidebar"
+                   tag="menu-sidebar"
+                   responsive
+                   [end]="sidebar.id === 'end'">
+        <nb-sidebar-header *ngIf="currentTheme !== 'corporate'">
+        </nb-sidebar-header>
+        <ng-content select="nb-menu"></ng-content>
+      </nb-sidebar>
+
       <nb-layout-column class="main-content">
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
