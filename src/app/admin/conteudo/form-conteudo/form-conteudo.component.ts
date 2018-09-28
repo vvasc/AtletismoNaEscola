d3d13829f3cb6ae9c38ca675207d60ff7616623a
Tonhao165 <eugenio165.as@gmail.com>
2018-09-28 01:50:45 -0300

    160318175: tslint

--- src/app/admin/conteudo/form-conteudo/form-conteudo.component.ts
+++ src/app/admin/conteudo/form-conteudo/form-conteudo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
