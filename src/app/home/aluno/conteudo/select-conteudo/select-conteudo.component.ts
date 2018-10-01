8d3031f613aea8f60209bb92f8ac8ec943110923
Vinícius <viniciuscmgo@gmail.com>
2018-10-01 17:20:51 -0300

    innerHTML da selecao de conteudo alterado via ts

--- src/app/home/aluno/conteudo/select-conteudo/select-conteudo.component.ts
+++ src/app/home/aluno/conteudo/select-conteudo/select-conteudo.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ConteudoService } from '../../../../services/conteudo.service';
@@ -10,20 +9,20 @@ import { ConteudoService } from '../../../../services/conteudo.service';
   styleUrls: ['./select-conteudo.component.scss'],
 })
 export class SelectConteudoComponent implements OnInit {
-  text: string = '';
+  texto;
+  @ViewChild('textohtml') textohtml: ElementRef;
   id: number;
 
   constructor(
     private conteudoService: ConteudoService,
     private activatedRoute: ActivatedRoute, // permite acessar o id em vigor na rota
-    protected sanitizer: DomSanitizer,
   ) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.id = params['id'];
       this.conteudoService.getConteudo(this.id).subscribe(conteudo => {
-        this.text = conteudo['texto'];
+        this.textohtml.nativeElement.innerHTML = conteudo['texto'];
       });
     });
   }
