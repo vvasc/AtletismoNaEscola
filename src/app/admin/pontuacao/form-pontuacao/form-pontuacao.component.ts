ecb39f4ecf367bbf91fe7c35106a989a7e16e4e0
Antonio Eugenio <eugenio165.as@gmail.com>
2018-10-03 11:08:57 -0300

    160676810: Form Pontuação criado

--- src/app/admin/pontuacao/form-pontuacao/form-pontuacao.component.ts
+++ src/app/admin/pontuacao/form-pontuacao/form-pontuacao.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ngx-form-pontuacao',
   templateUrl: './form-pontuacao.component.html',
   styleUrls: ['./form-pontuacao.component.scss'],
 })
-export class FormPontuacaoComponent implements OnInit {
+export class FormPontuacaoComponent implements OnInit, OnChanges {
+  @Input() atividade; // Recebe a atividade, no create pontuacao
+  @Input() aluno; // Recebe o aluno, no create pontuacao
+  @Input() pontuação; // Recebe a pontuacao, no caso da edit-pontuacao
+  @Output() formValue = new EventEmitter(); // Emite o valor do forms
+  formPontuacao: FormGroup;
 
-  constructor() { }
+  nomeAluno; // Usado para mostrar o nome do usuario, ja que o forms aceita apenas o ID
+  tituloAtividade; // Usado para mostrar o titulo da atividade, ja que o forms aceita apenas o ID
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formPontuacao = this.fb.group({
+      pontuacaoAula: ['', Validators.required],
+      aluno: ['', Validators.required],
+      atividade: ['', Validators.required],
+    });
+
+    this.formPontuacao.valueChanges.subscribe(value => {
+      this.formValue.emit(this.formPontuacao);
+    });
+  }
+
+  ngOnChanges(changes) {
+    if (changes.atividade && changes.atividade.currentValue) {
+      this.tituloAtividade = changes.atividade.currentValue.titulo;
+      this.formPontuacao.controls['atividade'].setValue(changes.atividade.currentValue.id);
+    }
+    if (changes.aluno && changes.aluno.currentValue) {
+      this.nomeAluno = changes.aluno.currentValue.fullName;
+      this.formPontuacao.controls['aluno'].setValue(changes.aluno.currentValue.id);
+    }
+    if (changes.pontuacao && changes.pontuacao.currentValue) {
+      this.nomeAluno = changes.pontuacao.aluno.fullName;
+      this.tituloAtividade = changes.pontuacao.atividade.titulo;
+      this.formPontuacao.setValue({
+        aluno: changes.pontuacao.aluno.id,
+        atividade: changes.pontuacao.atividade.id,
+        pontuacaoAula: changes.pontuacao.pontuacaoAula,
+      });
+    }
   }
 
 }
