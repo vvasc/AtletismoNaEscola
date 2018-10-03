0e4cff43cd9b3c60272b4d6c3f96920c0ddc2db0
Tonhao165 <eugenio165.as@gmail.com>
2018-10-03 16:07:12 -0300

    160676810: Create-pontuacao finalizado

--- src/app/admin/pontuacao/create-pontuacao/create-pontuacao.component.ts
+++ src/app/admin/pontuacao/create-pontuacao/create-pontuacao.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { NotificacaoService } from '../../../services/notificacao.service';
 import { AtividadeService } from '../../../services/atividade.service';
 import { map } from 'rxjs/operators';
+import { PontuacaoService } from '../../../services/pontuacao.service';
 
 @Component({
   selector: 'ngx-create-pontuacao',
@@ -13,18 +14,22 @@ import { map } from 'rxjs/operators';
 export class CreatePontuacaoComponent implements OnInit {
   alunosObs: Observable<any>; // Observable que contem todos os alunos, passado para a tabela
   atividadesObs: Observable<any>; // Observable com todas as atividades, passado para a tabela
-  alunoEscolhido; // Objeto que contem os dados do aluno escolhido
-  atividadeEscolhida; // Objeto que contem os dados da atividade escolhida
+  alunoEscolhido; // Objeto que contem os dados do aluno escolhido ao clicar na tabela
+  atividadeEscolhida; // Objeto que contem os dados da atividade escolhida ao clicar na tabela
+  querying: boolean = false;
+  formPontuacao; // Contem o form que veio do form component, contendo o ID do aluno e atividade e a pontuacao associada
 
   constructor(
     private accountservice: AccountService,
     private notificacao: NotificacaoService,
     private atividadeservice: AtividadeService,
+    private pontuacaoservice: PontuacaoService,
   ) { }
 
   ngOnInit() {
-    this.alunosObs = this.accountservice.getAllAccounts();
+    this.alunosObs = this.accountservice.getAllAccounts(); // Pega todos os alunos para passar para a tabela
     this.atividadesObs = this.atividadeservice.getAllAtividades().pipe(
+      // Pega todas as atividades para passar para a tabela
       map((atividades: any) => { // refatorando objeto para ser usado na table
         atividades.forEach(element => {
           element.titulopratica = element.provaPratica[0].titulo;
@@ -43,8 +48,22 @@ export class CreatePontuacaoComponent implements OnInit {
     this.atividadeEscolhida = event;
   }
 
-  resolveForm(form) {
-    console.log(form);
+  resolveForm(form) { // Recebe form emitido do forms acada mudança
+    this.formPontuacao = form;
+  }
+
+  criarPontuacao() {
+    this.querying = true;
+    this.pontuacaoservice.createPontuacao(this.formPontuacao.value).subscribe(sucesso => {
+      this.querying = false;
+      this.notificacao.ngxtoaster('SUCESSO!', 'Pontuação criada com sucesso!', true);
+      this.formPontuacao.reset();
+    }, err  => {
+      this.querying = false;
+      const errmsg = (err.status === 400) ?
+      'Já existe pontuacao dessa atividade para esse Aluno!' : 'Não foi possível criar a pontuação';
+      this.notificacao.ngxtoaster('ERRO!', errmsg, false);
+    });
   }
 
 }
--- src/app/admin/pontuacao/form-pontuacao/form-pontuacao.component.ts
+++ src/app/admin/pontuacao/form-pontuacao/form-pontuacao.component.ts
@@ -31,15 +31,15 @@ export class FormPontuacaoComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes) {
-    if (changes.atividade && changes.atividade.currentValue) {
+    if (changes.atividade && changes.atividade.currentValue) { // Se trocou a atividade, seta os campos correspondentes
       this.tituloAtividade = changes.atividade.currentValue.titulo;
       this.formPontuacao.controls['atividade'].setValue(changes.atividade.currentValue.id);
     }
-    if (changes.aluno && changes.aluno.currentValue) {
+    if (changes.aluno && changes.aluno.currentValue) { // Se trocou o aluno, seta os campos correspondentes
       this.nomeAluno = changes.aluno.currentValue.fullName;
       this.formPontuacao.controls['aluno'].setValue(changes.aluno.currentValue.id);
     }
-    if (changes.pontuacao && changes.pontuacao.currentValue) {
+    if (changes.pontuacao && changes.pontuacao.currentValue) { // Se trocou a pontuacao, seta os campos correspondentes
       this.nomeAluno = changes.pontuacao.aluno.fullName;
       this.tituloAtividade = changes.pontuacao.atividade.titulo;
       this.formPontuacao.setValue({
