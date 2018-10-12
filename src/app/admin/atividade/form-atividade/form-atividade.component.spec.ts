a5d94df55f88ad6294f44ec5b9f685f5eb633be4
joao vitor <94joaovitorls@gmail.com>
2018-10-11 21:45:53 -0300

    fix para fazer o pull request

--- src/app/admin/atividade/form-atividade/form-atividade.component.spec.ts
+++ src/app/admin/atividade/form-atividade/form-atividade.component.spec.ts
@@ -8,7 +8,7 @@ describe('FormAtividadeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormAtividadeComponent ]
+      declarations: [ FormAtividadeComponent ],
     })
     .compileComponents();
   }));
