1f8becb440cd4c5c47179ae43e0190785300156b
Luís Otávio <luis_villela3@hotmail.com>
2018-10-18 03:54:05 -0300

    correções 2
    por algum motivo esse arquivo search.componente estava com erro lint.

--- src/app/admin/pontuacao/edit-pontuacao/edit-pontuacao.component.ts
+++ src/app/admin/pontuacao/edit-pontuacao/edit-pontuacao.component.ts
@@ -47,7 +47,7 @@ export class EditPontuacaoComponent implements OnInit {
     (formval.owner === '') ? formval.owner = null : null;
     this.pontuacaoservice.patchPontuacao(this.pontuacaoselecionada.id, formval).subscribe(succ => {
       this.notificacao.ngxtoaster('Sucesso!', 'Pontuação Editada com Sucesso!', true);
-      this.refreshPontuacao();
+      // this.refreshPontuacao();
       this.formPontuacao.reset();
     }, err => {
       const errmsg = (err.error.code === 'E_UNIQUE') ? '' : 'Falha na conexão!';
@@ -74,5 +74,9 @@ export class EditPontuacaoComponent implements OnInit {
     this.formPontuacao = form;
   }
 
+  deletar(event) {
+    const pontuacaoNovaVal = -1;
+    this.pontuacaoservice.patchPontuacao(this.pontuacaoselecionada.id, pontuacaoNovaVal);
+  }
 
 }
